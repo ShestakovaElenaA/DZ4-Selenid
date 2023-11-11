caa9746df53466dad8a163590917232d2fe428ac
Elena Shestakova <ShestakovaElenaA@yandex.ru>
2023-11-11 08:22:21 +0300

    added test2

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -1,18 +1,1 @@
 package ru.netology;
-
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
-
-public class Main {
-    //Date dateNow = new Date();
-    SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy");
-    //Calendar meetingDate = Calendar.getInstance();
-    //meetingDate.setTime(dateNow);
-    //System.out.println(formatForDateNow.toString());
-    //meetingDate.add(Calendar.DAY_OF_MONTH,3);
-    //LocalDate date = LocalDate.now();
-    //LocalDate meetingDate = date.plusDays(3);
-    //String timeStamp = new SimpleDateFormat("yyyyMMdd").format(meetingDate);
-}
